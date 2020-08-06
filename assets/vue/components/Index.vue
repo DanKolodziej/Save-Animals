76f17dede73de8552b0e8c18206bb6169456f14a
DanKolodziej <Dank0l@o2.pl>
2020-08-06 21:56:55 +0200

    added index page text

--- assets/vue/components/Index.vue
+++ assets/vue/components/Index.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        INDEX
+    <div class="index">
+        Strona internetowa pomoc-zwierzakom.pl powstała w celu pomagania ludziom w adopcji, odnalezieniu zaginionych zwierzaków oraz informowaniu o zagrożonych gatunkach w Polsce.
+        Głównym celem jest stworzenie miejsca gdzie zgromadzone będą dane o jak największej liczbie różnych zwierzaków, które:
+        <ul>
+            <li>
+                różne osoby (zarówno osoby fizyczne jak i schroniska itp.) chciałyby oddać do adopcji
+            </li>
+            <li>
+                różne osoby chciałyby adoptować
+            </li>
+            <li>
+                zaginęły
+            </li>
+            <li>
+                zostały znalezione i szukają swojego właściciela.
+            </li>
+        </ul>
+        Dodatkowym celem jest promowanie informacji o zagrożonych gatunkach w Polsce poprzez udostępnienie danych na ich temat na podstawie wikipedii.
+        Strona ta została stworzona przeze mnie (czyli przez 1 osobę) jako osobisty projekt, który przy okazji będzie pożyteczny.
+        Wszelkie uwagi, problemy, porady, pytania itp. proszę wysyłać na email: pomoc.zwierzakom.kontakt@gmail.com
+
     </div>
 </template>
 
@@ -12,4 +31,7 @@
 
 <style scoped>
 
+    .index {
+        padding: 30px 5%;
+    }
 </style>
